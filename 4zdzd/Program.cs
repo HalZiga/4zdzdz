e9117a5df5e0d22577f9561ff7ab5ae6c07e48d9
HalZiga <113126146+HalZiga@users.noreply.github.com>
2022-10-07 23:45:55 +0300

    Добавьте файлы проекта.

---
+++ 4zdzd/Program.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _4zdzd
+{
+    internal class Program
+    {
+        static int zadanie(params int[] aray)
+        {
+            int Sum = 0;
+            for (int i = 0; i < aray.Length; i++)
+            {
+                Sum += aray[i];
+
+            }
+            return Sum;
+        }
+        static void zadanie1(ref int menat, params int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                menat *= array[i];
+            }
+        }
+        static void zadanie2(out double sred, params int[] array)
+        {
+            sred = array.Sum() / array.Length;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1");
+            Console.WriteLine("Введите коэффиценты a,b,c");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            double diskr = Math.Pow(b, 2) - 4 * a * c;
+            if (diskr > 0)
+            {
+                double x1 = (b * (-1) + Math.Sqrt(diskr)) / 2 * a;
+                double x2 = (b * (-1) - Math.Sqrt(diskr)) / 2 * a;
+                Console.WriteLine("Корни: {0}; {1}", x1, x2);
+            }
+            else if (diskr == 0)
+            {
+                double x1 = (b * (-1)) / 2 * a;
+                Console.WriteLine("Коренб: " + x1);
+            }
+            else
+            {
+                Console.WriteLine("нет вещественных корней");
+            }
+
+
+            Console.WriteLine("Задагие 2");
+            Random rand = new Random();
+            int[] myArray = new int[20];
+            for ( int i = 0; i < 20; i++)
+            {
+                myArray[i] = rand.Next(0,100);
+            }
+            Console.WriteLine(string.Join(" ", myArray));
+            Console.WriteLine("Выберете числa");
+            int index1 = Array.IndexOf(myArray, int.Parse(Console.ReadLine()));
+            int index2 = Array.IndexOf(myArray, int.Parse(Console.ReadLine()));
+            (myArray[index1], myArray[index2]) = (myArray[index2], myArray[index1]);
+            Console.WriteLine(string.Join(" ", myArray));
+
+
+            Console.WriteLine("Задание 3");
+            Console.WriteLine("Сколько чисел будем сортировать?");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите числа для сортировки:");
+            int[] myArra = new int[N];
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArra[i] = Convert.ToInt32(Console.ReadLine());//asdfghj
+            }
+            for (int i = 0; i < myArra.Length; i++)
+            {
+                for (int j = i + 1; j < myArra.Length; j++)
+                {
+                    if (myArra[i] > myArra[j])
+                    {
+                        (myArra[i], myArra[j]) = (myArra[j], myArra[i]);
+                    }
+                }
+            }
+
+
+            Console.WriteLine("Задание 4");
+            int[] Ugaga = { 12, 5};
+            Console.WriteLine(zadanie(Ugaga));
+            int umnoz = 1;
+            zadanie1(ref umnoz, Ugaga);
+            Console.WriteLine($"Произведение элементов массива = {umnoz}");
+            double sre = 0;
+            zadanie2(out sre, Ugaga);//Спросить почему, почему когда я указываю просто zadanie2(Ugaga), VC просит инициализировать out
+            Console.WriteLine($"Среднее арифметическое = {sre}");//Почему хотя всё double он выводит int овую воьмёрку
+            
+            
+
+            Console.WriteLine("Задание 5");
+            Console.WriteLine("Введите 1 для начала работы или 0 если не хотите начинать");
+            int vkl = int.Parse(Console.ReadLine());
+            if (vkl == 1)//Я проверяю хочет ли он чтобы программа работала
+            {
+                while (vkl == 1)//Чтобы вводить числа до того как не решат закрыть
+                {
+                    System.Threading.Thread.Sleep(2000);
+                    Console.Clear();
+                    Console.WriteLine("Введите число");
+                    string chislo = Console.ReadLine().ToLower();
+                    if (chislo == "exit" || chislo == "закрыть")
+                    {
+                        System.Environment.Exit(0);
+                    }
+                    else
+                    {
+                        try
+                        {
+                            string zghj = chislo;
+                            int peremen = Convert.ToInt32(zghj);
+                            switch (chislo)
+                            {
+                                case "0":
+                                    Console.WriteLine("###" + "\n" + "# #" + "\n" + "# #" + "\n" + "# #" + "\n" + "###");
+                                    break;
+                                case "1":
+                                    Console.WriteLine(" # " + "\n" + " # " + "\n" + " # " + "\n" + " # " + "\n" + "###");
+                                    break;
+                                case "2":
+                                    Console.WriteLine("###" + "\n" + "  #" + "\n" + "  #" + "\n" + "###" + "\n" + "#" + "\n" + "#" + "\n" + "###");
+                                    break;
+                                case "3":
+                                    Console.WriteLine("## " + "\n" + "  #" + "\n" + " # " + "\n" + "  #" + "\n" + "## ");
+                                    break;
+                                case "4":
+                                    Console.WriteLine("# #" + "\n" + "# #" + "\n" + "###" + "\n" + "  #" + "\n" + "###");
+                                    break;
+                                case "5":
+                                    Console.WriteLine("###" + "\n" + "#  " + "\n" + "###" + "\n" + "  #" + "\n" + "## ");
+                                    break;
+                                case "6":
+                                    Console.WriteLine(" ##" + "\n" + "#  " + "\n" + "###" + "\n" + "# #" + "\n" + "###");
+                                    break;
+                                case "7":
+                                    Console.WriteLine("###" + "\n" + "  #" + "\n" + "  #" + "\n" + " # " + "\n" + " # ");
+                                    break;
+                                case "8":
+                                    Console.WriteLine("###" + "\n" + "# #" + "\n" + " # " + "\n" + "# #" + "\n" + "###");
+                                    break;
+                                case "9":
+                                    Console.WriteLine("###" + "\n" + "# #" + "\n" + "###" + "\n" + "  #" + "\n" + "###");
+                                    break;
+                        
+                                default:
+                                    Console.BackgroundColor = ConsoleColor.Red;
+                                    Console.Clear();
+                                    Console.WriteLine("Ошибка!!!");
+                                    System.Threading.Thread.Sleep(3000);
+                                    Console.ResetColor();
+                                    Console.Clear();
+                                    break;
+                            }
+                        }
+                        catch (NullReferenceException e)
+                        {
+                            Console.WriteLine(e.Message);
+                            Environment.Exit(0);
+                        }
+                        catch (FormatException e)
+                        {
+                            Console.WriteLine(e.Message);
+                            Environment.Exit(0);
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Что-то не так");
+                            Environment.Exit(0);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                System.Environment.Exit(0);
+            }
+
+
+            Console.WriteLine("Задание 7");
+
+            int[] inputArray = { 9, 12, 9, 2, 17, 1, 6 };
+
+            int[] sortedArray = QuickSort(inputArray, 0, inputArray.Length - 1);
+
+            Console.WriteLine($"Sorted array: {string.Join(", ", sortedArray)}");
+
+            Console.ReadLine();
+            
+
+            Console.ReadKey();
+        }
+        private static int[] QuickSort(int[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+
+            int pivotIndex = GetPivotIndex(array, minIndex, maxIndex);
+
+            QuickSort(array, minIndex, pivotIndex - 1);
+
+            QuickSort(array, pivotIndex + 1, maxIndex);
+
+            return array;
+        }
+
+        private static int GetPivotIndex(int[] array, int minIndex, int maxIndex)
+        {
+            int pivot = minIndex - 1;
+
+            for (int i = minIndex; i <= maxIndex; i++)
+            {
+                if (array[i] < array[maxIndex])
+                {
+                    pivot++;
+                    Swap(ref array[pivot], ref array[i]);
+                }
+            }
+
+            pivot++;
+            Swap(ref array[pivot], ref array[maxIndex]);
+
+            return pivot;
+        }
+
+        private static void Swap(ref int leftItem, ref int rightItem)
+        {
+            int temp = leftItem;
+
+            leftItem = rightItem;
+
+            rightItem = temp;
+        }
+    }
+}
---
+++ tumachloh/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tumachloh
+{
+    internal class Program
+    {
+        static void Zad51(int a, int b)
+        {
+            if (a == b)
+            {
+                Console.WriteLine("Они равны");
+            }
+            else if (b < a)
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine(b);
+            }
+        }
+        static void Zad52(ref int a,ref int b)
+        {
+            (a, b) = (b, a);
+            Console.WriteLine($"{a} {b}");
+        }
+        static bool Zad53(ref int f)
+        {
+            int num = 1;
+            for (int i = 1; i <= f; i++)
+            {
+                try
+                {
+                    checked
+                    {
+                        num *= i;
+                    }
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return false;
+                }
+            }
+            f = num;
+            return true;
+        }
+        static void Zad54(int num, int nachal, int fakt)
+        {
+            num *= nachal;
+            if (nachal >= fakt)
+            {
+                Console.WriteLine($"Фактрориал = {num}");
+                return;
+            }
+            nachal++;
+            Zad54(num, nachal, fakt);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("тума 5.1");
+            Console.WriteLine("Введите а, потом б");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            //Zad51(a,b);
+
+
+            Console.WriteLine("тума 5.2");
+            Console.WriteLine("Введите а, потом б");
+            //Спросить можно ли ввести числа разделить сплитом как-то так(a, b) = int.Parse(Console.ReadLine().Split(' '));
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            Zad52(ref a,ref b);
+
+
+            Console.WriteLine("тума 5.3");
+            Console.WriteLine("Введите число");
+            int fak = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{Zad53(ref fak)}, Факториал = {fak}");
+
+            Console.WriteLine("тума 5.4");
+            Console.WriteLine("Введите число");
+            int fakt = int.Parse(Console.ReadLine());
+            Zad54(1, 1, fakt);
+
+
+            
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
